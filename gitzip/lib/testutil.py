9b02b892496db4cfaf90a4cd69b34b18abf84139
KD Bauer <kbauer@users.noreply.github.com>
2023-01-05 22:10:28 +0100

    testutil: Introduce chdir2tmp() and ls()

--- gitzip/lib/testutil.py
+++ gitzip/lib/testutil.py
@@ -1,10 +1,11 @@
 import os
 import tempfile
 from contextlib import contextmanager
+from os import mkdir, chdir
 from pathlib import Path
 from subprocess import check_call
 from tempfile import TemporaryDirectory
-from typing import Union, Iterator
+from typing import Union, Iterator, Optional, Iterable
 
 
 @contextmanager
@@ -53,27 +54,34 @@ def check_shell(*args, **kwargs) -> None:
     check_call(*args, shell=True, **kwargs)
 
 
-def create_files(*filenames: Union[str, Path], cwd: Path = None):
+def create_files(*filenames: str, cwd: Path = None):
     """
     Create empty files at the given names.
     If cwd is given, create them relative to that root.
+    A path ending in / can be used to create an empty directory.
 
         >>> tempdir = tempfile.TemporaryDirectory(prefix="create_files.")
-        >>> create_files("readme.md", "src/main.c", "src/main.h", cwd=tempdir.name)
-        >>> sorted(os.listdir(tempdir.name))
-        ['readme.md', 'src']
-
-        >>> sorted(os.listdir(tempdir.name + "/src"))
-        ['main.c', 'main.h']
+        >>> create_files("readme.md", "src/main.c", "src/main.h", "test/", cwd=tempdir.name)
+        >>> ls(tempdir.name, recursive=True)
+        readme.md
+        src/main.c
+        src/main.h
+        test/
     """
+    cwd: Path = (
+        Path.cwd() if cwd is None else
+        Path(cwd))
+
     for filename in filenames:
-        if cwd is not None:
-            filename = Path(cwd) / filename
-        Path(filename).parent.mkdir(parents=True, exist_ok=True)
-        Path(filename).touch(exist_ok=True)
+        full_path = cwd / filename
+        full_path.parent.mkdir(parents=True, exist_ok=True)
+        if filename.endswith("/"):
+            full_path.mkdir(exist_ok=True)
+        else:
+            full_path.touch(exist_ok=True)
 
 
-def make_temporary_directory(*args, **kwargs):
+def make_temporary_directory(*args, **kwargs) -> Path:
     """Wrapper around TemporaryDirectory, that keeps the directory
     alive until the end of execution and returns it as a Path object."""
     obj = TemporaryDirectory(*args, **kwargs)
@@ -82,3 +90,69 @@ def make_temporary_directory(*args, **kwargs):
 
 
 make_temporary_directory.items = set()
+
+
+def chdir2tmp(*args, **kwargs) -> None:
+    """
+    Create a temporary directory for the runtime of the program, and chdir to it.
+    Parameters the same as C(tempfile.TemporaryDirectory).
+    """
+    tmpdir = make_temporary_directory(*args, **kwargs)
+    os.chdir(tmpdir)
+
+
+def ls(
+        root: Optional[Union[str, Path]] = None,
+        recursive: bool = False,
+) -> None:
+    """
+    Print all the files in the directory for use in testing.
+
+    Consider an example directory
+
+        >>> chdir2tmp(prefix="dir_tree.")
+        >>> create_files("a.txt", "test/", "src/b.txt", "src/c.txt")
+
+    By default, the files in the current directory are printed,
+    with subdirectories indicated by trailing /.
+
+        >>> ls()
+        a.txt
+        src/
+        test/
+
+    With the 'recursive' option, the whole directory tree is printed.
+
+        >>> ls(recursive=True)
+        a.txt
+        src/b.txt
+        src/c.txt
+        test/
+
+    Paths are always printed relative to the root path
+
+        >>> oldpwd = Path.cwd()
+        >>> chdir("..")
+        >>> ls(oldpwd)
+        a.txt
+        src/
+        test/
+    """
+    root: Path = (
+        Path.cwd() if root is None else
+        Path(root))
+
+    def print_recursively(path: Path):
+        path_string: str = str(path.relative_to(root)).replace(os.sep, "/")
+        if not path.is_dir():
+            print(path_string)
+        else:
+            entries = sorted(path.glob("*"))
+            if entries and recursive:
+                for entry in entries:
+                    print_recursively(entry)
+            else:
+                print(path_string + "/")
+
+    for entry in root.glob("*"):
+        print_recursively(entry)
