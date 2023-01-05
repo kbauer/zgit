3a17aff5b1e14e64d95c047be85440c6a6ac307f
KD Bauer <kbauer@users.noreply.github.com>
2023-01-05 22:11:49 +0100

    Switch to "tar": This is present on Windows by default too now!

--- gitzip/commands/git_zip.py
+++ gitzip/commands/git_zip.py
@@ -3,7 +3,8 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 from inspect import cleandoc
 from os import listdir
 from pathlib import Path
-from subprocess import check_call
+from shutil import rmtree
+from subprocess import check_call, DEVNULL
 
 from gitzip.lib.gitutil import git_get_gitzip_file, git_get_root_directory
 from gitzip.lib.testutil import check_shell, create_files, make_temporary_directory
@@ -56,29 +57,50 @@ def do_pack():
 
         >>> repo_path = make_temporary_directory(prefix="do_pack.")
         >>> create_files("src/main.c", "src/main.h", cwd=repo_path)
-        >>> check_shell('git init && git add . && git commit -m "initial commit"', cwd=repo_path)
+        >>> check_shell('git init && git add . && git commit -m "initial commit"', cwd=repo_path, stdout=DEVNULL)
 
     Normally such git repositories are somewhat large.
 
         >>> assert len(list(repo_path.glob("**/*"))) > 20
 
-    After packing, only a single file is left:
+    After packing, only a single file is left,
+    while the working set is left unchanged:
 
-        >>> check_shell("git zip pack", cwd=repo_path)
+        >>> check_shell("git zip pack", cwd=repo_path, stdout=DEVNULL)
         >>> listdir(repo_path/".git")
-        ["gitzip.zip"]
+        ['gitzip.zip']
+        >>> listdir(repo_path)
+        ['.git', 'src']
+        >>> listdir(repo_path/'src')
+        ['main.c', 'main.h']
     """
     gitzip_file: Path = git_get_gitzip_file()
     git_root: Path = git_get_root_directory()
+    git_dir: Path = git_root / ".git"
 
     if gitzip_file.exists():
         logging.error("Already exists: %s", gitzip_file)
         exit(1)
 
+    # At least on windows, some files may be marked as read-only for some reason.
+    for file_path in git_dir.glob("**/*"):
+        file_path.chmod(0o777)
+
+    entries_to_add = list(git_dir.glob("*"))
     check_call(
-        ["zip", "-rm0", str(gitzip_file.relative_to(git_root)),
-         "--", *listdir(git_root)],
-        cwd=git_root)
+        ["tar", "czvf", str(gitzip_file.relative_to(git_dir)),
+         "--", *(str(p.relative_to(git_dir)) for p in entries_to_add)],
+        cwd=git_dir)
+    for path in entries_to_add:
+        if path.is_dir():
+            rmtree(path)
+        else:
+            path.unlink()
+
+
+def do_unpack():
+    gitzip_file: Path = git_get_gitzip_file()
+    git_root: Path = git_get_root_directory()
 
 
 if __name__ == "__main__":
